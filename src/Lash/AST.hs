6d96fa72487468da17b5e654a949db83d182978f
Sébastien Guimmara <sguimmara@pobox.com>
2017-07-18 21:23:13 +0200

    improve parser

--- src/Lash/AST.hs
+++ src/Lash/AST.hs
@@ -9,8 +9,26 @@ data Assignment = Assignment Name Word
 
 type Filename = Text
 
+data CommandList
+  = CommandList [Sequence] deriving (Eq, Show)
+
+data Sequence
+  = CondEnd Pipeline
+  | CondSeq Pipeline
+            ConditionalOperator
+            Sequence
+            deriving (Eq, Show)
+
+data ConditionalOperator = AndIf | OrIf deriving (Show, Eq)
+
+data Pipeline =
+  Pipeline Bool -- inverse ExitCode
+           [Command]
+           deriving (Eq, Show)
+
 data Command =
-  SimpleCommand Word -- command name
+  SimpleCommand [Assignment] -- variable assignments
+                (Maybe Word) -- command name
                 [Word] -- arguments
                 [IOFile] -- redirects
   deriving (Show, Eq)
@@ -37,6 +55,7 @@ getValue :: Word -> Text
 getValue EmptyWord = ""
 getValue (Quoted s) = s
 getValue (Unquoted s) = s
+getValue (SingleQuoted s) = s
 
 getValueS :: Word -> String
 getValueS = unpack . getValue
--- src/Lash/AST/Parser.hs
+++ src/Lash/AST/Parser.hs
@@ -1,23 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Lash.AST.Parser where
 
 import Lash.AST
 
-import           Data.Either    (rights, lefts)
-import           Data.Maybe     (catMaybes)
-import           Prelude hiding (Word)
+import Data.Either (rights, lefts)
+import Data.Maybe (catMaybes, isJust)
 import qualified Data.Text as T
-import           Text.Parsec as P
-import           Text.Parsec.Text
+import Prelude hiding (Word)
+import qualified Text.Parsec as P
+import Text.Parsec hiding (parse)
+import Text.Parsec.Text
 
 digits :: String
-digits = ['0'..'9']
+digits = ['0' .. '9']
 
 lowercaseLetters :: String
-lowercaseLetters = ['a'..'z']
+lowercaseLetters = ['a' .. 'z']
 
 uppercaseLetters :: String
-uppercaseLetters = ['A'..'Z']
+uppercaseLetters = ['A' .. 'Z']
 
 letters :: String
 letters = lowercaseLetters ++ uppercaseLetters
@@ -25,88 +27,136 @@ letters = lowercaseLetters ++ uppercaseLetters
 ioFile :: GenParser st IOFile
 ioFile = IOFile <$> optionMaybe fd <*> ioFileMode <*> word
 
-parse :: T.Text -> Either ParseError Command
-parse s = P.parse simpleCommand "" s
+parseUnsafe :: T.Text -> Sequence
+parseUnsafe s = let (Right x) = parse s in x
+
+parse :: T.Text -> Either ParseError Sequence
+parse = P.parse condSeq ""
+
+commandList :: GenParser st CommandList
+commandList = CommandList <$> sepBy condSeq separator
+
+separator :: GenParser st ()
+separator = spaces >> (char '&' <|> char ';') >> return ()
+
+condSeq :: GenParser st Sequence
+condSeq =
+  choice
+    [ try $ CondSeq <$> pipeline <*> condOp <*> condSeq
+    , try $ CondEnd <$> pipeline]
+
+condSeqElmt :: GenParser st (Pipeline, Maybe ConditionalOperator)
+condSeqElmt = do
+  p <- pipeline
+  c <- optionMaybe condOp
+  return (p, c)
+
+condOp :: GenParser st ConditionalOperator
+condOp = spaces >> (orIf <|> andIf)
+
+orIf :: GenParser st ConditionalOperator
+orIf = string "||" >> return OrIf
+
+andIf :: GenParser st ConditionalOperator
+andIf = string "&&" >> return AndIf
+
+pipeline :: GenParser st Pipeline
+pipeline = do
+  spaces
+  bang <- optionMaybe (char '!')
+  spaces
+  cmds <- sepBy command pipeSymbol
+  return (Pipeline (isJust bang) cmds)
+
+pipeSymbol :: GenParser st ()
+pipeSymbol = spaces >> char '|' >> spaces >> return ()
+
+command :: GenParser st Command
+command = spaces >> simpleCommand
 
 simpleCommand :: GenParser st Command
 simpleCommand = do
-    cmdName <- word
-    iow <- many ioFileOrWord
+  ioa <- many (try ioFileOrAssignment)
+  cmdName <- optionMaybe word
+  iow <- many ioFileOrWord
+  return $
+    SimpleCommand (rights ioa) cmdName (rights iow) (lefts iow ++ lefts ioa)
 
-    return $ SimpleCommand cmdName (rights iow) (lefts iow)
+ioFileOrAssignment :: GenParser st (Either IOFile Assignment)
+ioFileOrAssignment = (Right <$> assignment) <|> (Left <$> ioFile)
 
 ioFileOrWord :: GenParser st (Either IOFile Word)
-ioFileOrWord = do
-    spaces
-    x <- optionMaybe ioFile
-    case x of
-        Just iof -> return $ Left iof
-        Nothing -> Right <$> word
+ioFileOrWord =  (Left <$> ioFile) <|> (Right <$> word)
 
 fd :: GenParser st Int
 fd = do
-    d <- many1 $ oneOf digits
-    return (read d :: Int)
+  d <- many1 $ oneOf digits
+  return (read d :: Int)
 
 ioFileMode :: GenParser st IOFileMode
 ioFileMode = do
-    spaces
-    c <- oneOf "><"
-    c' <- optionMaybe $ oneOf "><|&"
-    let xs = catMaybes [Just c, c']
-    case xs of
-        ">" -> return IOTo
-        "<" -> return IOFrom
-        ">|" -> return IOClobber
-        ">>" -> return IOAppend
-        "<>" -> return IOReadWrite
-        "<&" -> return IOFromBoth
-        ">&" -> return IOToBoth
-        _ -> error "just to appease the type checker"
+  c <- oneOf "><"
+  c' <- optionMaybe $ oneOf "><|&"
+  let xs = catMaybes [Just c, c']
+  spaces
+  case xs of
+    ">" -> return IOTo
+    "<" -> return IOFrom
+    ">|" -> return IOClobber
+    ">>" -> return IOAppend
+    "<>" -> return IOReadWrite
+    "<&" -> return IOFromBoth
+    ">&" -> return IOToBoth
+    _ -> error "just to appease the type checker"
 
 assignment :: GenParser st Assignment
 assignment = do
-    spaces
-    n <- name
-    _ <- char '='
-    ow <- optionMaybe word
-    case ow of
-        Just w -> return (Assignment n w)
-        Nothing -> return (Assignment n EmptyWord)
+  spaces
+  n <- name
+  _ <- char '='
+  ow <- optionMaybe word
+  case ow of
+    Just w -> return (Assignment n w)
+    Nothing -> return (Assignment n EmptyWord)
 
 name :: GenParser st Name
 name = do
-    let initChars = ('_':letters)
-    x <- oneOf initChars
-    xs <- many $ oneOf (digits ++ initChars)
-    return $ Name (T.pack (x:xs))
+  let initChars = ('_' : letters)
+  x <- oneOf initChars
+  xs <- many $ oneOf (digits ++ initChars)
+  return $ Name (T.pack (x : xs))
 
 word :: GenParser st Word
-word = spaces >> (quoted <|> unquoted)
+word = do
+  spaces
+  w <- (quoted <|> unquoted)
+  spaces
+  return w
 
 quoted :: GenParser st Word
 quoted = do
-    c <- oneOf ['"', '\'']
-    s <- manyTill quotedWordChar (char c)
-    case s of
-        [] -> return EmptyWord
-        _  -> case c of
-            '\'' -> return $ SingleQuoted (T.pack s)
-            '\"' -> return $ Quoted (T.pack s)
+  c <- oneOf ['"', '\'']
+  s <- manyTill quotedWordChar (char c)
+  case s of
+    [] -> return EmptyWord
+    _ ->
+      if c == '\''
+        then return $ SingleQuoted (T.pack s)
+        else return $ Quoted (T.pack s)
 
 unquoted :: GenParser st Word
 unquoted = many1 wordChar >>= \s -> return $ Unquoted (T.pack s)
 
 quotedWordChar :: GenParser st Char
 quotedWordChar = do
-    c <- noneOf "\t\r"
-    case c of
-        '\\' -> anyChar
-        _ -> return c
+  c <- noneOf "\t\r"
+  case c of
+    '\\' -> anyChar
+    _ -> return c
 
 wordChar :: GenParser st Char
 wordChar = do
-    c <- noneOf "; \t\r\n\"'"
-    case c of
-        '\\' -> anyChar
-        _ -> return c
+  c <- noneOf "><&|; \t\r\n\"'"
+  case c of
+    '\\' -> anyChar
+    _ -> return c
